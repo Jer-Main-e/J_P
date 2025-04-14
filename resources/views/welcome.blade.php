2ded49fbefe2a0d0cfa2753562e9dfe8f0ffb8a1
Jermaine Tjen-a-Kwoei <s1211196@student.windesheim.nl>
2025-04-14 15:31:06 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jermaine's ポケモン</title>
+</head>
+<body>
+    <h1>ポケモン head</h1>
+    <p>Click on the button below to view a list of ポケモン</p>
+    <a href="" class="btn">
+        Find ポケモン
+    </a>
+</body>
+</html>
